7b9beddfcee90b45deda565ab4abb6c51b6c2577
bchiang7 <brittany.chiang@gmail.com>
2016-12-11 18:22:50 -0500

    remove _site from gitignore

---
+++ _site/v1/js/scrolling-nav.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(".navbar").offset().top>50?$(".navbar-fixed-top").addClass("top-nav-collapse"):$(".navbar-fixed-top").removeClass("top-nav-collapse")}),$(function(){$("a.page-scroll").bind("click",function(o){var a=$(this);$("html, body").stop().animate({scrollTop:$(a.attr("href")).offset().top},1500,"easeInOutExpo"),o.preventDefault()})});
