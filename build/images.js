ea2515df7ef2138ed6396b0211890ef2fb3986ea
Brittany Chiang <brittany.chiang@gmail.com>
2018-10-03 22:20:28 -0400

    Update a few packages

--- build/images.js
+++ build/images.js
@@ -4,11 +4,10 @@ const imgPath = 'img/**/*.+(png|jpg|gif|svg)';
 const destPath = '_site/img';
 
 module.exports = gulp => {
-
   gulp.task('images', () => {
-    return gulp.src(imgPath)
+    return gulp
+      .src(imgPath)
       .pipe(imagemin())
       .pipe(gulp.dest(destPath));
   });
-
 };
