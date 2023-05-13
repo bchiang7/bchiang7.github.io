b1004d01625f57b27d3800bc30e945542cbefb76
AlexAnth <anthiswebdev@gmail.com>
2023-05-13 18:59:08 +0300

    Minor

--- js/main.js
+++ js/main.js
@@ -72,6 +72,6 @@ $(function() {
   sr.reveal('.background');
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
-  sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.05 });
+  sr.reveal('.featured-projects', { viewFactor: 0.2 });
+  sr.reveal('.other-projects', { viewFactor: 0.2 });
 });
